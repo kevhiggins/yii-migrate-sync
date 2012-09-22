92668126309215732c21e53ad3f22ffd91e22a3b
Kevin Higgins <kev.higgins@gmail.com>
2012-09-22 15:05:15 -0500

    Feeling out the class.

--- protected/commands/MigrateExtendedCommand.php
+++ protected/commands/MigrateExtendedCommand.php
@@ -7,15 +7,84 @@ class MigrateExtendedCommand extends MigrateCommand
 	
 	public function actionSync($args)
 	{
+		$this->checkTables();
+		
+		// Foreach sync table
+			// If does not exist in dbTables
+				// Add delete table migration
+		
+		
+		
+		// Foreach table
+		// Create all new tables
+		
+		// Delete all deleted tables
+		
+		// Forech 
+		
+		//$tables = Yii::app()->db->schema->getTables();
+		
+	}
+	
+	protected function checkTables()
+	{
 		$db = $this->getDbConnection();
 		$syncDb = $this->getSyncDbConnection();
 		
 		$dbTables = $db->schema->getTables();
 		$syncDbTables = $syncDb->schema->getTables();
 		
-		//$tables = Yii::app()->db->schema->getTables();
-		CVarDumper::dump();	
+		// Find New Tables
+		$newTables = array_diff_key($dbTables, $syncDbTables);
+		foreach($newTables as $newTable)
+			$this->addTableMigration($newTable);		
+		
+		// Find Deleted Tables
+		$deletedTables = array_diff_key($syncDbTables, $dbTables);
+		foreach($deletedTables as $deletedTable)
+		{
+			$this->deleteTableMigration($deletedTable);
+		}
+		
+		// Find Shared Tables
+		$sharedTables = array_intersect_key($dbTables, $syncDbTables);
+		foreach($sharedTables as $sharedTable)
+		{
+			$this->checkColumns($sharedTable);
+		}
+		
+	//	CVarDumper::dump($deletedTables);
 	}
+	
+	protected function checkColumns($table)
+	{
+		
+	}
+	
+	protected function addTableMigration($table)
+	{
+		
+	}
+	
+	protected function deleteTableMigration()
+	{
+		
+	}
+	
+	protected function createColumnMigration()
+	{
+		
+	}
+	
+	protected function alterColumnMigration()
+	{
+		
+	}
+	
+	// Check indexes
+	
+	// Check keys
+	
 	
 	/**
 	 * @var CDbConnection
